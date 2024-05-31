e4392edf783e3f52555393d61a19be15251e2d5f
Alex Tsang <alextsang@Alexs-MacBook-Pro.local>
2024-05-31 16:25:20 -0500

    use aws server backend endpoint

--- src/Api/axiosInstance.tsx
+++ src/Api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BACKEND_API_URL = "https://backend.guilder-invest.com:8443";
-const BACKEND_API_URL = "http://localhost:5173";
+const BACKEND_API_URL = "https://backend.guilder-invest.com:8443";
+// const BACKEND_API_URL = "http://localhost:5173";
 
 const axiosInstance = axios.create({
   baseURL: BACKEND_API_URL,
