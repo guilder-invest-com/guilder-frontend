cce56ddcff4e44470dd01e0cef42253336c5198e
Alex Tsang <alextsang@Alexs-MacBook-Pro.local>
2024-06-06 18:06:27 -0500

    change backendapiurl

--- src/Api/axiosInstance.tsx
+++ src/Api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BACKEND_API_URL = "https://backend.guilder-invest.com:8443";
-const BACKEND_API_URL = "http://localhost:3500";
+const BACKEND_API_URL = "https://backend.guilder-invest.com:8443";
+// const BACKEND_API_URL = "http://localhost:3500";
 
 const axiosInstance = axios.create({
   baseURL: BACKEND_API_URL,
